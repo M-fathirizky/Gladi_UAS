08851fcb59e6c44deff8a9bf28686fa3adc5303d
M-fathirizky <fathirrizky636@gmail.com>
2026-06-27 18:00:57 +0700

    Fix formatting in Main.java login output

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -15,6 +15,7 @@ public class Main {
         while (true) {
             User user = auth.login();
             System.out.println(user != null ? "Login berhasil: " + user.getUsername() : "");
+            
 
             if (user == null) break;
 
